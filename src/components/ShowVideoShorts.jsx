666547a4682d90b4418bf0425bd7ed6344f673a3
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-02-12 06:28:59 +0330

    mod menu items page shertsvideo

--- src/components/ShowVideoShorts.jsx
+++ src/components/ShowVideoShorts.jsx
@@ -39,19 +39,127 @@ function ShowVideoShorts() {
     { id: 9, name: "9", ico: imgreact },
   ];
   const listcoments = [
-    {id:1, imgprof:imgreact , user:'@abolfzl-1', textcoment:'tstcoment-1' , comentlike:10,comentdislike:50 , date:'1 months ago',replys:[]},
-    {id:2, imgprof:imgreact , user:'@abolfzl-2', textcoment:'tstcoment-2' , comentlike:45,comentdislike:3 , date:'2 months ago',replys:[]},
-    {id:3, imgprof:imgreact , user:'@abolfzl-3', textcoment:'tstcoment-3' , comentlike:23,comentdislike:30 , date:'3 months ago',replys:[]},
-    {id:4, imgprof:imgreact , user:'@abolfzl-4', textcoment:'tstcoment-4' , comentlike:98,comentdislike:5 , date:'4 months ago',replys:[]},
-    {id:5, imgprof:imgreact , user:'@abolfzl-5', textcoment:'tstcoment-5' , comentlike:21,comentdislike:32 , date:'5 months ago',replys:[]},
-    {id:6, imgprof:imgreact , user:'@abolfzl-6', textcoment:'tstcoment-6' , comentlike:90,comentdislike:45 , date:'6 months ago',replys:[]},
-    {id:7, imgprof:imgreact , user:'@abolfzl-7', textcoment:'tstcoment-7' , comentlike:130,comentdislike:12 , date:'7 months ago',replys:[]},
-    {id:8, imgprof:imgreact , user:'@abolfzl-8', textcoment:'tstcoment-8' , comentlike:40,comentdislike:39 , date:'8 months ago',replys:[]},
-    {id:9, imgprof:imgreact , user:'@abolfzl-9', textcoment:'tstcoment-9' , comentlike:120,comentdislike:2 , date:'9 months ago',replys:[]},
-    {id:10, imgprof:imgreact , user:'@abolfzl-10', textcoment:'tstcoment-10' , comentlike:730,comentdislike:8 , date:'10 months ago',replys:[]},
-    {id:11, imgprof:imgreact , user:'@abolfzl-11', textcoment:'tstcoment-11' , comentlike:260,comentdislike:26 , date:'11 months ago',replys:[]},
-    {id:12, imgprof:imgreact , user:'@abolfzl-12', textcoment:'tstcoment-12' , comentlike:430,comentdislike:16 , date:'12 months ago',replys:[]},
-  ]
+    {
+      id: 1,
+      imgprof: imgreact,
+      user: "@abolfzl-1",
+      textcoment: "tstcoment-1",
+      comentlike: 10,
+      comentdislike: 50,
+      date: "1 months ago",
+      replys: [],
+    },
+    {
+      id: 2,
+      imgprof: imgreact,
+      user: "@abolfzl-2",
+      textcoment: "tstcoment-2",
+      comentlike: 45,
+      comentdislike: 3,
+      date: "2 months ago",
+      replys: [],
+    },
+    {
+      id: 3,
+      imgprof: imgreact,
+      user: "@abolfzl-3",
+      textcoment: "tstcoment-3",
+      comentlike: 23,
+      comentdislike: 30,
+      date: "3 months ago",
+      replys: [],
+    },
+    {
+      id: 4,
+      imgprof: imgreact,
+      user: "@abolfzl-4",
+      textcoment: "tstcoment-4",
+      comentlike: 98,
+      comentdislike: 5,
+      date: "4 months ago",
+      replys: [],
+    },
+    {
+      id: 5,
+      imgprof: imgreact,
+      user: "@abolfzl-5",
+      textcoment: "tstcoment-5",
+      comentlike: 21,
+      comentdislike: 32,
+      date: "5 months ago",
+      replys: [],
+    },
+    {
+      id: 6,
+      imgprof: imgreact,
+      user: "@abolfzl-6",
+      textcoment: "tstcoment-6",
+      comentlike: 90,
+      comentdislike: 45,
+      date: "6 months ago",
+      replys: [],
+    },
+    {
+      id: 7,
+      imgprof: imgreact,
+      user: "@abolfzl-7",
+      textcoment: "tstcoment-7",
+      comentlike: 130,
+      comentdislike: 12,
+      date: "7 months ago",
+      replys: [],
+    },
+    {
+      id: 8,
+      imgprof: imgreact,
+      user: "@abolfzl-8",
+      textcoment: "tstcoment-8",
+      comentlike: 40,
+      comentdislike: 39,
+      date: "8 months ago",
+      replys: [],
+    },
+    {
+      id: 9,
+      imgprof: imgreact,
+      user: "@abolfzl-9",
+      textcoment: "tstcoment-9",
+      comentlike: 120,
+      comentdislike: 2,
+      date: "9 months ago",
+      replys: [],
+    },
+    {
+      id: 10,
+      imgprof: imgreact,
+      user: "@abolfzl-10",
+      textcoment: "tstcoment-10",
+      comentlike: 730,
+      comentdislike: 8,
+      date: "10 months ago",
+      replys: [],
+    },
+    {
+      id: 11,
+      imgprof: imgreact,
+      user: "@abolfzl-11",
+      textcoment: "tstcoment-11",
+      comentlike: 260,
+      comentdislike: 26,
+      date: "11 months ago",
+      replys: [],
+    },
+    {
+      id: 12,
+      imgprof: imgreact,
+      user: "@abolfzl-12",
+      textcoment: "tstcoment-12",
+      comentlike: 430,
+      comentdislike: 16,
+      date: "12 months ago",
+      replys: [],
+    },
+  ];
 
   const setlikefunc = () => {
     if (!likeflag) {
@@ -118,13 +226,13 @@ function ShowVideoShorts() {
     };
   });
 
-  const subscribefunc =()=>{
-    if(sub){
-      setdialogsub(true)
-    }else{
-      setsub(true)
+  const subscribefunc = () => {
+    if (sub) {
+      setdialogsub(true);
+    } else {
+      setsub(true);
     }
-  }
+  };
 
   return (
     <div className="w-[450px] sm:w-[550px] md:w-[750px] md:flex md:justify-center h-[620px] overflow-y-auto shareshortspage overflow-scroll">
@@ -259,9 +367,64 @@ function ShowVideoShorts() {
             <div
               ref={divshowmenu}
               style={{ display: "none" }}
-              className=" text-black w-[300px] h-[200px] z-[100] absolute flexv flex-col bottom-28 right-3 rounded-xl bg-gray-200 shadow-lg shadow-gray-800 "
+              className=" text-black w-[300px] h-max z-[100] absolute flex flex-col gap-2 p-2 bottom-28 right-3 rounded-xl bg-gray-200 shadow-lg shadow-gray-800 "
             >
-              tstttssttststtdbvssts
+              <button className="flex gap-1 items-center hover:bg-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ display: "block", width: "25px", height: "25px" }}
+                  focusable="false"
+                >
+                  <path d="M21 6H3V5h18v1zm-8 3-9.99.01V10L13 9.99V9zm8 4H3v1h18v-1zm-8 4H3v1h10v-1z"></path>
+                </svg>
+                Description
+              </button>
+
+              <button className="flex gap-1 items-center hover:bg-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  style={{ display: "block", width: "25px", height: "25px" }}
+                >
+                  <path d="M4 20h14v1H3V6h1v14zm14-10h-4V6h-1v4H9v1h4v4h1v-4h4v-1zm3-7v15H6V3h15zm-1 1H7v13h13V4z"></path>
+                </svg>
+                Save to playlist
+              </button>
+
+              <button className="flex gap-1 items-center hover:bg-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  style={{ display: "block", width: "25px", height: "25px" }}
+                >
+                  <path d="M6 14v-4c0-.55.45-1 1-1h3c.55 0 1 .45 1 1v1H9.5v-.5h-2v3h2V13H11v1c0 .55-.45 1-1 1H7c-.55 0-1-.45-1-1zm8 1h3c.55 0 1-.45 1-1v-1h-1.5v.5h-2v-3h2v.5H18v-1c0-.55-.45-1-1-1h-3c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm6-11H4v16h16V4m1-1v18H3V3.01C3 3 3 3 3.01 3H21z"></path>
+                </svg>
+                Captions
+              </button>
+
+              <button className="flex gap-1 items-center hover:bg-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  style={{ display: "block", width: "25px", height: "25px" }}
+                >
+                  <g>
+                    <path d="M12 3c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9m0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm7 11H5v-2h14v2z"></path>
+                  </g>
+                </svg>
+                Don't recommend this channel
+              </button>
+
+              <button className="flex gap-1 items-center hover:bg-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  focusable="false"
+                  style={{ display: "block", width: "25px", height: "25px" }}
+                >
+                  <path d="m13.18 4 .24 1.2.16.8H19v7h-5.18l-.24-1.2-.16-.8H6V4h7.18M14 3H5v18h1v-9h6.6l.4 2h7V5h-5.6L14 3z"></path>
+                </svg>
+                Report
+              </button>
             </div>
           </div>
 
@@ -271,7 +434,12 @@ function ShowVideoShorts() {
               <NavLink className=" text-white" to={`/`}>
                 abolfazl-tst
               </NavLink>
-              <button onClick={subscribefunc} className={` ${sub ? ' bg-[#25232385] text-white' : ' bg-yellow-50'} px-2 py-1  rounded-2xl `}>
+              <button
+                onClick={subscribefunc}
+                className={` ${
+                  sub ? " bg-[#25232385] text-white" : " bg-yellow-50"
+                } px-2 py-1  rounded-2xl `}
+              >
                 Subscribe
               </button>
             </div>
@@ -298,12 +466,10 @@ function ShowVideoShorts() {
 
             <div className=" w-full h-[450px] overflow-scroll widthscroll overflow-x-auto bg-red-100f">
               <div className="flex flex-col gap-2">
-              {
-                listcoments && listcoments.map(item=>
-                  <Comments key={item.id} coment={item} />
-                )
-              }
-
+                {listcoments &&
+                  listcoments.map((item) => (
+                    <Comments key={item.id} coment={item} />
+                  ))}
               </div>
             </div>
           </div>
@@ -357,19 +523,34 @@ function ShowVideoShorts() {
       )}
 
       {/* dialog-sub */}
-      {
-        dialogsub ? 
+      {dialogsub ? (
         <div className=" w-full h-full fixed z-[200] top-0 left-0 flex justify-center items-center bg-[#00000036] ">
           <div className="w-[270px] h-[100px] p-2 flex flex-col gap-3 justify-center bg-[#f5f5f8] shadow-lg shadow-gray-800 rounded-2xl">
-            <span className=" w-max mx-auto"> UnSubscribe from @abolfazl-tst?</span>
+            <span className=" w-max mx-auto">
+              {" "}
+              UnSubscribe from @abolfazl-tst?
+            </span>
             <div className=" w-full flex justify-center gap-5">
-            <button onClick={()=>setdialogsub(false)} className=" px-2 py-1 rounded-lg hover:bg-gray-200 text-black">Cancel</button>
-            <button onClick={()=>{setsub(false),setdialogsub(false)}} className=" px-2 py-1 rounded-lg hover:bg-gray-200 text-blue-600">UnSubscribe</button>
+              <button
+                onClick={() => setdialogsub(false)}
+                className=" px-2 py-1 rounded-lg hover:bg-gray-200 text-black"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => {
+                  setsub(false), setdialogsub(false);
+                }}
+                className=" px-2 py-1 rounded-lg hover:bg-gray-200 text-blue-600"
+              >
+                UnSubscribe
+              </button>
             </div>
           </div>
         </div>
-        : <></>
-      }
+      ) : (
+        <></>
+      )}
     </div>
   );
 }

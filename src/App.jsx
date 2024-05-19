243e12a49166069f839d1e46342cc03228dfcbad
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-05-20 01:17:05 +0330

    add aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useRoutes } from "react-router-dom"
 import routes from './route'
+import "aos/dist/aos.css"
+import { useEffect } from "react"
+import Aos from "aos"
 
 
 export default function App() {
+  // useEffect(()=>{
+  // Aos.init()
+  // },[])
   let router = useRoutes(routes)
   return router
 }
